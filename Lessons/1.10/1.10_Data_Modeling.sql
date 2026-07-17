d99a4b6fdbf8fbf79d6ac37805cb5f482267ccfc
Jason Loera <74157071+jloera82@users.noreply.github.com>
2026-07-16 18:46:19 -0700

    Rename column in company_dim query

--- Lessons/1.10/1.10_Data_Modeling.sql
+++ Lessons/1.10/1.10_Data_Modeling.sql
@@ -2,7 +2,7 @@ SELECT job_id,job_title_short,salary_year_avg,company_id
 FROM job_postings_fact
 LIMIT 10;
 
-SELECT company_id, name
+SELECT company_id, name AS company_name
 FROM company_dim
 LIMIT 10;
 
